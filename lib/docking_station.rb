e5db881fb6b23ea9038de017f272d46746827c5d
Lauren Ashton <l.v.ashton@outlook.com>
2021-09-23 17:34:17 +0100

    Completed sections 15 and 16

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,17 +1,30 @@
 class DockingStation
   attr_reader :bikes
+  DEFAULT_CAPACITY = 20
 
   def initialize
     @bikes = []
   end
 
   def release_bike
-    raise "No bikes available" if @bikes.empty?
+    raise "No bikes available" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise "Dock is full" if @bikes.length == 20
+    raise "Dock is full" if full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    return true if bikes.length >= DEFAULT_CAPACITY
+    false
+  end
+
+  def empty?
+    return true if bikes.length == 0
+    false
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,7 @@ require "./lib/docking_station.rb"
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-  
+
   it "releases a docked bike" do
     station = DockingStation.new
     bike = Bike.new
@@ -18,17 +18,17 @@ describe DockingStation do
 
   describe "#dock" do
     it { is_expected.to respond_to(:dock).with(1).arguments } # query necessary?
-    
+
     it "docks a bike" do
       station = DockingStation.new
       bike = Bike.new
       station.dock(bike)
       expect(station.bikes).to include(bike)
     end
-    
+
     it "raises an error when the docking station is full" do
       station = DockingStation.new
-      20.times { station.dock(Bike.new) }
+      DockingStation::DEFAULT_CAPACITY.times { station.dock(Bike.new) }
       expect { station.dock(Bike.new) }.to raise_error("Dock is full")
     end
   end
